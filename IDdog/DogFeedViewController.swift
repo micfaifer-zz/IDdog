f1d6825b65f78819e57a85064bd2db8af4774bff
Michelle Faifer <micfaifer@gmail.com>
2018-09-13 12:27:26 -0300

    Added simple storyboards layout

---
+++ IDdog/DogFeedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DogFeedViewController.swift
+//  IDdog
+//
+//  Created by lugia on 13/09/18.
+//  Copyright © 2018 MicFaifer. All rights reserved.
+//
+
+import UIKit
+
+class DogFeedViewController: UIViewController {
+
+    // MARK: - Outlets
+
+    @IBOutlet weak var dogFeedCollectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ IDdog/SignUpViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SignUpViewController.swift
+//  IDdog
+//
+//  Created by lugia on 11/09/18.
+//  Copyright © 2018 MicFaifer. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    // MARK: - Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+
+    @IBAction func signUp(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
