d0d58d95cde4bdf65ea0c87aed333cac41a04bdf
Michelle Faifer <micfaifer@gmail.com>
2018-09-13 17:14:52 -0300

    DogFeed result model

---
+++ IDdog/Model/DogFeedResult.swift
@@ -0,0 +1,16 @@
+//
+//  DogFeedResult.swift
+//  IDdog
+//
+//  Created by lugia on 13/09/18.
+//  Copyright © 2018 MicFaifer. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+struct DogFeedResult: Codable {
+    let category: String
+    let list: [String]
+}
